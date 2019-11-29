f3a5a8c1ff9ca6ed8286034a6294638db06d238c
jere <jere@packhaus.info>
2019-11-29 15:19:17 +0100

    Add Colors to RecPlots

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from collections import defaultdict
 import re
 from praatclasses import TextGrid
 from transitions.extensions import GraphMachine as Machine
+from PIL import Image, ImageDraw
 
 VP_WORDS_PATH = os.path.join('VPs', 'Words')
 VP_BLICKRICHTUNGEN_PATH = os.path.join('VPs', 'Blickrichtungen')
@@ -19,10 +20,11 @@ ANALYSEN_PATH = 'Analysen'
 CSV_PATH = 'csv'
 GRAPH_PATH = 'graphs'
 RECURRENCE_PATH = 'recPlots'
+NUMBER2COLOR = {0: (102, 102, 102), 1: (0, 204, 255), 2: (0, 0, 255), 3: (0, 0, 128), 4: (102, 255, 51), 5: (0, 255, 0),
+                6: (0, 128, 0), 7: (255, 128, 128), 8: (255, 0, 0), 9: (128, 0, 0)}
 
 
 def analyze_eye_movement_patterns(interval_tier):
-
     pattern_dict = {}
     for n in range(0, 10):
         pattern_dict[n] = defaultdict((int))
@@ -49,12 +51,10 @@ def analyze_eye_movement_patterns(interval_tier):
                 del interval
                 continue
 
-
-
     return pattern_dict
 
 
-def compute_relative_frequencies(pattern_dict, withFive = True):
+def compute_relative_frequencies(pattern_dict, withFive=True):
     for blickrichtung in pattern_dict:
         sum = 0
         for key, v in pattern_dict[blickrichtung].items():
@@ -83,7 +83,7 @@ def write_movementpattern_to_csv(filename, pattern_dict, withFive=True):
             writer.writerow([str(blickrichtung)] + row)
 
 
-def create_transition_graph_from_dict(pattern_dict, withFive = True):
+def create_transition_graph_from_dict(pattern_dict, withFive=True):
     if withFive:
         machine = Machine(states=[str(x) for x in pattern_dict.keys()], use_pygraphviz=False)
 
@@ -97,7 +97,6 @@ def create_transition_graph_from_dict(pattern_dict, withFive = True):
                 continue
             if pattern_dict[blickrichtung][next_blckrchtng] == 0:
                 continue
-
 
             machine.add_transition(str(pattern_dict[blickrichtung][next_blckrchtng]), str(blickrichtung),
                                    str(next_blckrchtng))
@@ -119,7 +118,7 @@ def create_recurrence_plot_from_intervaltier(interval_tier, destination, withFiv
     for key in dic:
         if key == 0:
             continue
-        if dic[key] == dic[key-1]:
+        if dic[key] == dic[key - 1]:
             to_remove.append(key)
     for n in to_remove:
         del dic[n]
@@ -139,7 +138,6 @@ def create_recurrence_plot_from_intervaltier(interval_tier, destination, withFiv
     result.min_diagonal_line_length = 2
     result.min_vertical_line_length = 2
     result.min_white_vertical_line_lelngth = 2
-    print(result)
     with open(destination + "_recAnal.txt", mode='w') as file:
         file.write(str(result))
 
@@ -148,15 +146,37 @@ def create_recurrence_plot_from_intervaltier(interval_tier, destination, withFiv
     ImageGenerator.save_recurrence_plot(result.recurrence_matrix_reverse,
                                         destination + "_recPlot.png")
 
+    add_numbers_to_recurrence_plot(data_points, destination + "_recPlot.png")
+
+
 def cleanup_IntervalTier(intervals):
-
-
     # change interval marks to literals where applicable
     for interval in intervals:
         if len(interval.mark()) > 1:
             interval.change_text(interval.mark()[0])
 
-def do_Analysis(withFive = True):
+
+def add_numbers_to_recurrence_plot(numbers, recPlot):
+    plotIm = Image.open(recPlot)
+    newPlotIm = Image.new(plotIm.mode, (plotIm.width + 1, plotIm.height + 1), color='white')
+    newPlotIm.paste(plotIm, box=(1, 0))
+
+    colordict = defaultdict(list)
+
+    for number in NUMBER2COLOR:
+        colordict[NUMBER2COLOR[number]] = []
+
+    for number in range(len(numbers)):
+        colordict[NUMBER2COLOR[int(numbers[number])]] += [(0, (newPlotIm.height-2) - (number)), (number+1, newPlotIm.height-1)]
+
+    plotDraw = ImageDraw.Draw(newPlotIm)
+
+    for color in colordict:
+         plotDraw.point(colordict[color], color)
+
+    newPlotIm.save(recPlot[:-4] + "_numbered.png")
+
+def do_Analysis(withFive=True):
     regex = r'(\d*_*vp\d*)_.*\.TextGrid'
 
     VP_TextGrids = dict()
@@ -186,9 +206,8 @@ def do_Analysis(withFive = True):
 
     for vp_nr in VP_TextGrids:
         create_recurrence_plot_from_intervaltier(VP_TextGrids[vp_nr][0], os.path.join(ANALYSEN_PATH, RECURRENCE_PATH,
-                                                                                         vp_nr), withFive)
+                                                                                      vp_nr), withFive)
 
 
 if __name__ == '__main__':
-
     do_Analysis(withFive=True)
